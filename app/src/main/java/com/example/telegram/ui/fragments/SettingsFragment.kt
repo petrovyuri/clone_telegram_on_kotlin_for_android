689fe79ac873b2fb68506838263c40ce2f61eb90
mrdevgo <mrdevgo@gmail.com>
2020-03-16 20:19:31 +0300

    Урок 7. Добавил в экран настроек меню с иконками

--- app/src/main/java/com/example/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/telegram/ui/fragments/SettingsFragment.kt
@@ -1,12 +1,17 @@
 package com.example.telegram.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.example.telegram.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
-
+        setHasOptionsMenu(true)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
+    }
 }
