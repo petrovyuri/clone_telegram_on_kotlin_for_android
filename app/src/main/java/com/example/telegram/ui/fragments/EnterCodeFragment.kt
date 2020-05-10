d1b45e36938a71cf56dc70b30ad3389783a884f2
mrdevgo <mrdevgo@gmail.com>
2020-05-10 09:19:18 +0300

    31. Считываем номера телефонов из базы данных

--- app/src/main/java/com/example/telegram/ui/fragments/EnterCodeFragment.kt
+++ app/src/main/java/com/example/telegram/ui/fragments/EnterCodeFragment.kt
@@ -37,13 +37,17 @@ class EnterCodeFragment(val phoneNumber: String, val id: String) :
                 dateMap[CHILD_PHONE] = phoneNumber
                 dateMap[CHILD_USERNAME] = uid
 
-                REF_DATABASE_ROOT.child(NODE_USERS).child(uid).updateChildren(dateMap)
-                    .addOnCompleteListener { task2 ->
-                        if (task2.isSuccessful) {
-                            showToast("Добро пожаловать")
-                            (activity as RegisterActivity).replaceActivity(MainActivity())
-                        } else showToast(task2.exception?.message.toString())
+                REF_DATABASE_ROOT.child(NODE_PHONES).child(phoneNumber).setValue(uid)
+                    .addOnFailureListener { showToast(it.message.toString()) }
+                    .addOnSuccessListener {
+                        REF_DATABASE_ROOT.child(NODE_USERS).child(uid).updateChildren(dateMap)
+                            .addOnSuccessListener {
+                                showToast("Добро пожаловать")
+                                (activity as RegisterActivity).replaceActivity(MainActivity())
+                            }
+                            .addOnFailureListener { showToast(it.message.toString()) }
                     }
+
             } else showToast(task.exception?.message.toString())
         }
     }
--- app/src/main/java/com/example/telegram/utilits/appDatabaseHelper.kt
+++ app/src/main/java/com/example/telegram/utilits/appDatabaseHelper.kt
@@ -12,6 +12,7 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import java.util.ArrayList
 
 lateinit var AUTH: FirebaseAuth
 lateinit var CURRENT_UID: String
@@ -21,8 +22,10 @@ lateinit var USER: User
 
 const val NODE_USERS = "users"
 const val NODE_USERNAMES = "usernames"
-
+const val NODE_PHONES = "phones"
+const val NODE_PHONES_CONTACTS = "phones_contacts"
 const val FOLDER_PROFILE_IMAGE = "profile_image"
+
 
 
 const val CHILD_ID = "id"
@@ -44,7 +47,7 @@ fun initFirebase() {
 }
 
 
-inline fun putUrlToDatabase(url: String, crossinline  function: () -> Unit) {
+inline fun putUrlToDatabase(url: String, crossinline function: () -> Unit) {
     /* Функция высшего порядка, отпраляет полученый URL в базу данных */
     REF_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_UID)
         .child(CHILD_PHOTO_URL).setValue(url)
@@ -52,14 +55,14 @@ inline fun putUrlToDatabase(url: String, crossinline  function: () -> Unit) {
         .addOnFailureListener { showToast(it.message.toString()) }
 }
 
-inline fun getUrlFromStorage(path: StorageReference,crossinline  function: (url: String) -> Unit) {
+inline fun getUrlFromStorage(path: StorageReference, crossinline function: (url: String) -> Unit) {
     /* Функция высшего порядка, получает  URL картинки из хранилища */
     path.downloadUrl
         .addOnSuccessListener { function(it.toString()) }
         .addOnFailureListener { showToast(it.message.toString()) }
 }
 
-inline fun putImageToStorage(uri: Uri, path: StorageReference,crossinline function: () -> Unit) {
+inline fun putImageToStorage(uri: Uri, path: StorageReference, crossinline function: () -> Unit) {
     /* Функция высшего порядка, отправляет картинку в хранилище */
     path.putFile(uri)
         .addOnSuccessListener { function() }
@@ -72,16 +75,16 @@ inline fun initUser(crossinline function: () -> Unit) {
     REF_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_UID)
         .addListenerForSingleValueEvent(AppValueEventListener {
             USER = it.getValue(User::class.java) ?: User()
-            if (USER.username.isEmpty()){
+            if (USER.username.isEmpty()) {
                 USER.username = CURRENT_UID
             }
             function()
         })
 }
 
- fun initContacts() {
-     /* Функция считывает контакты с телефонной книги, хаполняет массив arrayContacts моделями CommonModel */
-    if (checkPermission(READ_CONTACTS)){
+fun initContacts() {
+    /* Функция считывает контакты с телефонной книги, хаполняет массив arrayContacts моделями CommonModel */
+    if (checkPermission(READ_CONTACTS)) {
         var arrayContacts = arrayListOf<CommonModel>()
         val cursor = APP_ACTIVITY.contentResolver.query(
             ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
@@ -91,16 +94,34 @@ inline fun initUser(crossinline function: () -> Unit) {
             null
         )
         cursor?.let {
-            while (it.moveToNext()){
+            while (it.moveToNext()) {
                 /* Читаем телефонную книгу пока есть следующие элементы */
-                val fullName = it.getString(it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
-                val phone = it.getString(it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                val fullName =
+                    it.getString(it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                val phone =
+                    it.getString(it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
                 val newModel = CommonModel()
                 newModel.fullname = fullName
-                newModel.phone = phone.replace(Regex("[\\s,-]"),"")
+                newModel.phone = phone.replace(Regex("[\\s,-]"), "")
                 arrayContacts.add(newModel)
             }
         }
         cursor?.close()
+        updatePhonesToDatabase(arrayContacts)
     }
+}
+
+fun updatePhonesToDatabase(arrayContacts: ArrayList<CommonModel>) {
+    REF_DATABASE_ROOT.child(NODE_PHONES).addListenerForSingleValueEvent(AppValueEventListener{
+        it.children.forEach {snapshot ->
+            arrayContacts.forEach {contact ->
+                if (snapshot.key == contact.phone){
+                    REF_DATABASE_ROOT.child(NODE_PHONES_CONTACTS).child(CURRENT_UID)
+                        .child(snapshot.value.toString()).child(CHILD_ID)
+                        .setValue(snapshot.value.toString())
+                        .addOnFailureListener { showToast(it.message.toString()) }
+                }
+            }
+        }
+    })
 }
