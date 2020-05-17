9d7d074987d423118b91daa94c5c9fe2e8a4d837
mrdevgo <mrdevgo@gmail.com>
2020-05-17 18:13:16 +0300

    34.Устранение утечки памяти

--- app/src/main/java/com/example/telegram/ui/fragments/ContactsFragment.kt
+++ app/src/main/java/com/example/telegram/ui/fragments/ContactsFragment.kt
@@ -22,6 +22,8 @@ class ContactsFragment : BaseFragment(R.layout.fragment_contacts) {
     private lateinit var mAdapter: FirebaseRecyclerAdapter<CommonModel, ContactsHolder>
     private lateinit var mRefContacts: DatabaseReference
     private lateinit var mRefUsers: DatabaseReference
+    private lateinit var mRefUsersListener:AppValueEventListener
+    private  var mapListeners = hashMapOf<DatabaseReference,AppValueEventListener>()
 
     override fun onResume() {
         super.onResume()
@@ -56,14 +58,15 @@ class ContactsFragment : BaseFragment(R.layout.fragment_contacts) {
             ) {
                 mRefUsers = REF_DATABASE_ROOT.child(NODE_USERS).child(model.id)
 
-                mRefUsers.addValueEventListener(AppValueEventListener {
+                mRefUsersListener = AppValueEventListener {
                     val contact = it.getCommonModel()
                     holder.name.text = contact.fullname
                     holder.status.text = contact.state
                     holder.photo.downloadAndSetImage(contact.photoUrl)
-                })
+                }
 
-
+                mRefUsers.addValueEventListener(mRefUsersListener)
+                mapListeners[mRefUsers] = mRefUsersListener
             }
         }
 
@@ -82,6 +85,11 @@ class ContactsFragment : BaseFragment(R.layout.fragment_contacts) {
     override fun onPause() {
         super.onPause()
         mAdapter.stopListening()
+        println()
+        mapListeners.forEach {
+            it.key.removeEventListener(it.value)
+        }
+        println()
     }
 }
 
